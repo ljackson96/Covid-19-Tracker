944fdf8cc4e99527461cd1a05065ffb84dbee61b
l.jackson96 <l.jackson96@outlook.com>
2020-05-23 14:39:55 -0700

    fetched api data for tracker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css'
+import { fetchData } from './api'
 export class App extends Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+        
+        console.log(data);
+    }
+
     render() {
         return (
             <div className={styles.container}>
